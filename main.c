63cfec6f528bbf2050223f9ff38b1b39a09c155c
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-11 04:24:57 +0100

    Add: further time config, openfile func, sendkey func

--- main.c
+++ main.c
@@ -1,25 +1,42 @@
 #include <windows.h>
 #include <stdio.h>
-
-
 #define WINVER 0x0500 // Tells windows we are win 2000 or later
 
 
-    // initialise input
+// ip.ki.wVk = 0x41; 
+// ip.ki.dwFlags = 0; 
+// SendInput(1, &ip, sizeof(INPUT));
 
-    // ip.ki.wVk = 0x41; 
-    // ip.ki.dwFlags = 0; 
-    // SendInput(1, &ip, sizeof(INPUT));
 
+
+void sendKey(INPUT input, int key){
+
+    input.ki.wVk = key; 
+    input.ki.dwFlags = 0; 
+    SendInput(1, &input, sizeof(INPUT));
+
+}
+
+FILE* openFile(char* path){
+
+    FILE *fptr;
+    fptr = fopen(path,"r");
+    return fptr;
+
+}
 
 int main(int argc, char *argv[]){
 
+    int time = 0;
+
     if(argc < 2 || argc > 3){
-        printf("USAGE: main.exe [filePath] [sleepTimer]\n");
+        printf("USAGE: main.exe [filePath] [sleepTimer : seconds]\n");
         return -1;
     }
 
-    // Sleep(5000);
+    time = (argc == 3) ? atoi(argv[2])*1000 : 5000;
+
+    Sleep(time);
 
     // initialise input
 
@@ -29,7 +46,6 @@ int main(int argc, char *argv[]){
     ip.ki.time = 0;
     ip.ki.dwExtraInfo = 0;
     
-
 
 
     return 0;
