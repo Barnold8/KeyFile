97c127da4d73f0c09f7f3a37d452809843c66d84
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-11 03:43:09 +0100

    Add: force CLI arg

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
 #include <windows.h>
+#include <stdio.h>
+
 
 #define WINVER 0x0500 // Tells windows we are win 2000 or later
+
+
+
+int main(int argc, char *argv[]){
+
+    if(argc != 1){
+        return -1;
+    }
+
+
+    return 0;
+}
