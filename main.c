80a8f9e91b951eedbb1887511eb62e8bcb67f968
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-11 04:15:57 +0100

    Add: provide sleep timer configuration

--- main.c
+++ main.c
@@ -5,15 +5,32 @@
 #define WINVER 0x0500 // Tells windows we are win 2000 or later
 
 
+    // initialise input
+
+    // ip.ki.wVk = 0x41; 
+    // ip.ki.dwFlags = 0; 
+    // SendInput(1, &ip, sizeof(INPUT));
+
 
 int main(int argc, char *argv[]){
 
-    if(argc != 2){
-        printf("%d\n",argc);
+    if(argc < 2 || argc > 3){
+        printf("USAGE: main.exe [filePath] [sleepTimer]\n");
         return -1;
     }
 
-    printf("%s\n", argv[1]);
+    // Sleep(5000);
+
+    // initialise input
+
+    INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0;
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+    
+
+
 
     return 0;
 }
