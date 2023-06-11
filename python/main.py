dd105ba2429dc12174b1d523915ad00536c8797c
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-11 20:41:15 +0100

    Add: full implementation of python alternative

--- python/main.py
+++ python/main.py
@@ -3,6 +3,8 @@ import sys
 import os
 import time
 
+wait_timer = 0
+
 try:
     import pyautogui
 except ImportError:
@@ -13,3 +15,18 @@ if len(sys.argv) < 2:
     print("USAGE: main.py [filePath] [sleepTimer : seconds]\n")
     exit(1)
 
+if len(sys.argv) >= 3:
+    try:
+        wait_timer = int(sys.argv[2])
+    except TypeError: # coudlnt find a casting error
+        wait_timer = 0 # ignore error and proceed as normal
+
+time.sleep(wait_timer)
+
+try:
+    with open(sys.argv[1],"r") as file:
+        for sentence in file.readlines():
+            pyautogui.write(sentence)
+except FileNotFoundError as fileError:
+    print(fileError)
+    exit(1)
