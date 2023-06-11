624b887982fadcee9fcceef0f8fd770e2dbec149
Brandon Wright <psybw7@nottingham.ac.uk>
2023-06-11 20:21:18 +0100

    Add: requirements.txt and import exception handling

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 # Python implementation, substitute of compiled program.
 import sys
-import pyautogui
+import os
+import time
+
+try:
+    import pyautogui
+except ImportError:
+    os.start("pip install -r requirements.txt")
+
+
+if len(sys.argv) < 2:
+    print("USAGE: main.py [filePath] [sleepTimer : seconds]\n")
+    exit(1)
+
